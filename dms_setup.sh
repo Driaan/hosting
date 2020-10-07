2d40b1c78cd47bf98b0ddc4ae0c9122f0612e7b5
Driaan <debeste.driaan@gmail.com>
2020-10-07 16:31:38 +0200

    Update dms_setup.sh

--- dms_setup.sh
+++ dms_setup.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
+#!/usr/bin/env bash
+set -e;
 echo "Enter DMS ID:"
 read dmsid
 echo "*/15 * * * * /usr/bin/curl -d s=$? https://nosnch.in/${dmsid} &> /dev/null" | sudo tee /home/driaan/dms
